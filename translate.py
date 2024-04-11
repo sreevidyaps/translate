a1b8c872eec38c9e8a868876de9502d77847b0b6
sreevidyaps <sreevidyasree2002@gmail.com>
2024-04-11 12:37:00 +0530

    re

--- translate.py
+++ translate.py
@@ -2,11 +2,11 @@ import streamlit as st
 from googletrans import Translator
 
 st.header("Machine Translation Demo")
-input=st.text_area("Please enter the text", value="")
-option=st.selectbox(
+input=st.text_area("Please enter the text", value="")  #can write multiple lines in text area
+option=st.selectbox( #dropdown with multiple options
     "To which language you wish to translate this text to?",
     ("Malayalam", "Hindi", "Tamil"))
-if st.button("Translate"):
+if st.button("Translate"): #if translate button is clicked
     translator=Translator()
-    translation=translator.translate(input,dest=option)
+    translation=translator.translate(input,dest=option) #get text from input and the language to be translated from option
     st.write(translation.text)
